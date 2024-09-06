e3b4641736861d7332749e2f9600a91d03462870
JeanetteSchwarz <jeanette.schwarz@t-online.de>
2024-09-06 09:42:03 +0200

    bug fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,32 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
 
-        ShopService myShop = new ShopService();
+        OrderRepo orderRepo = new OrderMapRepo();
         ProductRepo productRepo = new ProductRepo();
-        OrderMapRepo orderRepo = new OrderMapRepo();
         IdService idService = new IdService();
+        OrderListRepo orderListRepo = new OrderListRepo();
 
+        productRepo.addProduct(new Product("1", "Birne"));
+        productRepo.addProduct(new Product("2", "Banane"));
+        productRepo.addProduct(new Product("3", "Kiwi"));
+        productRepo.addProduct(new Product("4", "Nektarine"));
+
+        ShopService myShop = new ShopService(productRepo, orderRepo, idService);
+
+        myShop.addOrder(List.of("1"));
+        myShop.addOrder(List.of("1", "2", "3"));
+        myShop.addOrder(List.of("2", "4"));
+
+        //System.out.println("ProductRepo: " + productRepo);
+        //System.out.println("All Orders: " + orderRepo);
+        System.out.println(productRepo);
 
 
 
     }
+
+
+
 }
